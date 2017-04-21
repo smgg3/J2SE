65b9e8238c5ab16380aa58d47110cf972d5052da
Sravani Muraonda <murakondasravani64@gmail.com>
2017-04-20 21:19:14 -0500

    simple change

--- CoreJava/src/com/test/inheritence/HomeWork.java
+++ CoreJava/src/com/test/inheritence/HomeWork.java
@@ -5,6 +5,7 @@ public class HomeWork {
 	public static void main(String[] args) {
 		System.out.println("4/16\n1.Find whether the given number is prime or not?\n2. print prime numbers till the given numbers from 1?\n");
 		System.out.println("4/18\n1.Find whether the given number is even or odd?\n2.  print fibanocchi series of n count\n3.Find whether the given number is palindrome or not\n4.Tackle with switch operator and break statements\n5. try nested triple/control block operators?\n6.Can we write conditions in switch operator cases?\n7.study about splunk(log),aws,kibana(log),newrelic(performance),appdynamic(performance)s,jprofiler(performance),jrockit(performance)?\n");
+		System.out.println("4/20\n1.Create a calculator\n2. regular it has sum,difference,product\n3.scientific calculator should have sum,diff,product,square,cube\n");
 
 	}
 
